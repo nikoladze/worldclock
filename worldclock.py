fed1e99d22a96988b7f5f139a24f1fc5e28a4ead
Nikolai Hartmann <nikoladze@posteo.de>
2022-10-27 18:54:22 +0200

    split into functions

--- worldclock.py
+++ worldclock.py
@@ -36,6 +36,44 @@ def format_utcoffset(tz, reftime):
         utcoffset = f"-{midnight - utcoffset:%H:%M}"
     return utcoffset
 
+
+def all_timezones():
+    return ZoneInfoFile(getzoneinfofile_stream()).zones.keys()
+
+
+def print_timezones(reftime):
+    for timezone_str in all_timezones():
+        tz = dateutil.tz.gettz(timezone_str)
+        utcoffset = format_utcoffset(tz, reftime)
+        print(f"{timezone_str:<30} UTC{utcoffset}")
+
+
+def print_table(timezones, reftime, long=False):
+    timezones_per_utcoffset = defaultdict(list)
+    utcoffset_for_timezone = {}
+    for timezone_str in all_timezones():
+        tz = dateutil.tz.gettz(timezone_str)
+        timezones_per_utcoffset[format_utcoffset(tz, reftime)].append(timezone_str)
+        utcoffset_for_timezone[timezone_str] = format_utcoffset(tz, reftime)
+
+    max_len_also = 120
+    header = f"{'Name':<20} {'Abbr':<5} {'UTC offset':<10} {'Time':<18} {'Same time also in':<{max_len_also+4}}"
+    print(header)
+    print("=" * len(header))
+    for abbr, timezone in timezones.items():
+        tz = dateutil.tz.gettz(timezone)
+        if tz is None:
+            tz = dateutil.parser.parse("00:00 " + timezone).tzinfo
+        utcoffset = format_utcoffset(tz, reftime)
+        info = f"({abbr:<6} UTC{utcoffset})"
+        also_in_str = " ".join(sorted(timezones_per_utcoffset[utcoffset]))
+        if len(also_in_str) > max_len_also and not long:
+            also_in_str = also_in_str[:max_len_also] + "..."
+        print(
+            f"{timezone:<20} {abbr:<5} UTC{utcoffset:<7} {reftime.astimezone(tz):%Y-%m-%d %H:%M}    {also_in_str}"
+        )
+
+
 def main(args=None):
     timezones = TIMEZONES.copy()
 
@@ -46,12 +84,7 @@ def main(args=None):
     parser.add_argument("--list-timezones", action="store_true")
     args = parser.parse_args(args)
 
-    timezones_for_parser = {k: dateutil.tz.gettz(v) for k, v in TIMEZONES.items()}
-
-    if args.extra_list is not None:
-        for abbr in args.extra_list:
-            timezones[abbr] = abbr
-
+    timezones_for_parser = {k: dateutil.tz.gettz(v) for k, v in timezones.items()}
     if len(args.time) > 0:
         time = " ".join(args.time)
         reftime = dateutil.parser.parse(time, tzinfos=timezones_for_parser)
@@ -60,34 +93,16 @@ def main(args=None):
     if reftime.tzinfo is None:
         reftime = reftime.replace(tzinfo=dateutil.tz.tzlocal())
 
-    all_zones = ZoneInfoFile(getzoneinfofile_stream()).zones.keys()
-    timezones_per_utcoffset = defaultdict(list)
-    utcoffset_for_timezone = {}
-    for timezone_str in all_zones:
-        tz = dateutil.tz.gettz(timezone_str)
-        timezones_per_utcoffset[format_utcoffset(tz, reftime)].append(timezone_str)
-        utcoffset_for_timezone[timezone_str] = format_utcoffset(tz, reftime)
-
     if args.list_timezones:
-        for timezone, utcoffset in sorted(utcoffset_for_timezone.items(), key=lambda x: x[0]):
-            print(f"{timezone:<30} UTC{utcoffset}")
-        exit()
+        print_timezones(reftime)
+        return
 
-    max_len_also = 120
-    header = f"{'Name':<20} {'Abbr':<5} {'UTC offset':<10} {'Time':<18} {'Same time also in':<{max_len_also+4}}"
-    print(header)
-    print("=" * len(header))
-    for abbr, timezone in timezones.items():
-        tz = dateutil.tz.gettz(timezone)
-        if tz is None:
-            tz = dateutil.parser.parse("00:00 "+timezone).tzinfo
-        utcoffset = format_utcoffset(tz, reftime)
-        info = f"({abbr:<6} UTC{utcoffset})"
-        also_in_str = " ".join(sorted(timezones_per_utcoffset[utcoffset]))
-        if len(also_in_str) > max_len_also and not args.long:
-            also_in_str = also_in_str[:max_len_also] + "..."
-        print(f"{timezone:<20} {abbr:<5} UTC{utcoffset:<7} {reftime.astimezone(tz):%Y-%m-%d %H:%M}    {also_in_str}")
+    if args.extra_list is not None:
+        for abbr in args.extra_list:
+            timezones[abbr] = abbr
+
+    print_table(timezones, reftime, long=args.long)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
